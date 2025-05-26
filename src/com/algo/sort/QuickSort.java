72653aa6af5f9f22973cd10d0cf42ee62c1ac697
Haridath Bodapati <haridath@gmail.com>
2025-05-26 11:57:58 +0100

    Changes

--- src/com/algo/sort/QuickSort.java
+++ src/com/algo/sort/QuickSort.java
@@ -17,7 +17,7 @@ public class QuickSort {
 		int pivot = arr[high];
 		int i = low - 1;
 
-		for (int j = low; j < high; j++) {
+		for (int j = low; j <= high; j++) {
 			if (arr[j] < pivot) {
 				i++;
 				swap(arr, i, j);
